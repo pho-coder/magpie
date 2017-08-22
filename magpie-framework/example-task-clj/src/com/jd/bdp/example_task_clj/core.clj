8de6a00b84a8ef2aecf53084de4b49fa214919c5
Phoenix Zhang <pho.coderzhang@gmail.com>
2017-08-22 15:17:20 +0800

    add mac sigar

--- magpie-framework/example-task-clj/src/com/jd/bdp/example_task_clj/core.clj
+++ magpie-framework/example-task-clj/src/com/jd/bdp/example_task_clj/core.clj
@@ -4,17 +4,27 @@
             [clojure.tools.logging :as log]))
 
 (defn run-fn [job-id]
-  (log/info "HI:" job-id)
+  (log/info "run-fn" job-id)
   (Thread/sleep 3000))
 
 (defn prepare-fn [job-id]
-  (log/info "start" job-id))
+  (log/info "prefare-fn" job-id)
+  (Thread/sleep 1000))
 
 (defn reload-fn [job-id]
-  (log/info "reload" job-id))
+  (log/info "reload-fn" job-id)
+  (Thread/sleep 1000))
+
+(defn pause-fn [job-id]
+  (log/info "pause-fn" job-id)
+  (Thread/sleep 1000))
+
+(defn close-fn [job-id]
+  (log/info "close-fn" job-id)
+  (Thread/sleep 1000))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (log/info "Hello, World!")
-  (execute run-fn :prepare-fn prepare-fn))
+  (execute run-fn :prepare-fn prepare-fn :reload-fn reload-fn :pause-fn pause-fn :close-fn close-fn))
